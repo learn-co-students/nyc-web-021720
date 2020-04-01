2cdb4a100e5b464a2f70a3b02f726deb2e39de0b
Steven Doran <steven.doran@flatironschool.com>
2020-04-01 12:56:13 -0400

    post lecture

--- 24-intro-to-events/listeners.js
+++ 24-intro-to-events/listeners.js
@@ -3,8 +3,8 @@ console.log("listening")
 // when a user clicks on the welcome image it should display a baby sloth
 
 // √get the image
-// add a click listener
-// when clicked, change the picture
+// √add a click listener
+// √when clicked, change the picture
 
 let image = document.getElementById('welcome-image')
 
@@ -18,3 +18,15 @@ image.addEventListener("click", function(event){
 // thick turquoise border to the image when the mouse
 // hovers over it, and removes the border when the mouse is 
 // no longer hovering over it
+
+image.addEventListener('mouseover', function(event){
+  let image = event.target
+  console.dir(event.target)
+  image.style.border = "20px solid red"
+})
+
+image.addEventListener('mouseleave', function(event){
+  let image = event.target
+  console.dir(event.target)
+  image.style.border = ""
+})
