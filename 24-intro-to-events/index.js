a1926d89c546933a5dbea6d91f2f50533746a6dd
Steven Doran <steven.doran@flatironschool.com>
2020-04-01 11:06:13 -0400

    mid lecture

--- 24-intro-to-events/index.js
+++ 24-intro-to-events/index.js
@@ -31,5 +31,42 @@ const movies = [
   }
 ]
 
+// add Back to the Future to our DOM
 
+// √create an li for the new movie
+// √add class to li
+// √add HTML to the li
+// √get the ul
+// √attach this li to the ul on the page
+
+let backToTheFuture = {
+  title: 'Back to the Future',
+  year: 1985,
+  score: 99,
+  imageUrl: "https://cdn.shopify.com/s/files/1/1148/8924/products/MPW-93254-a_1024x1024.jpg?v=1571439877"
+}
+
+let ul = document.querySelector('ul')
+let li = createMovieEl(backToTheFuture)
+
+ul.appendChild(li)
+
+function createMovieEl(movieObj){
+  let newMovie = document.createElement('li')
+
+  newMovie.className = "movie"
+  
+  newMovie.innerHTML = `
+    <h3>${movieObj.title}</h3>
+    <img alt="" src="${movieObj.imageUrl}" />
+    <h4>Year: </h4>
+    <p>${movieObj.year}</p>
+    <h4>Score: <span>${movieObj.score}</span> </h4>
+    <button class="up-vote">Up Vote</button>
+    <button>Down Vote</button>
+    <button data-purpose="delete">&times;</button>
+  `
+
+  return newMovie
+}
 
