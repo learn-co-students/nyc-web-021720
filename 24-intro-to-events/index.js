b1e2ae7813441fe0125974fe3952e9776aa01a5d
Steven Doran <steven.doran@flatironschool.com>
2020-04-01 10:03:22 -0400

    pre lecture

---
+++ 24-intro-to-events/index.js
@@ -0,0 +1,35 @@
+const movies = [
+  {
+    title: 'The Goonies',
+    imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/515DYf99zfL.jpg',
+    year: 1985,
+    score: 0
+  },
+  { 
+    title: 'Free Willy',
+    imageUrl: 'https://upload.wikimedia.org/wikipedia/en/thumb/b/b5/Free_willy.jpg/220px-Free_willy.jpg',
+    year: 1993,
+    score: 0  
+  },
+  { 
+    title: 'Top Gun',
+    imageUrl: 'https://m.media-amazon.com/images/M/MV5BZjQxYTA3ODItNzgxMy00N2Y2LWJlZGMtMTRlM2JkZjI1ZDhhXkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_.jpg',
+    year: 1986,
+    score: 0  
+  },
+  { 
+    title: 'Mean Girls',
+    imageUrl: 'https://img01.mgo-images.com/image/thumbnail?id=1MV270609a1c6c89af5538a6d63cea71ed4&ql=70&sizes=310x465',
+    year: 2004,
+    score: 0  
+  },
+  { 
+    title: 'Parasite',
+    imageUrl: 'https://mymodernmet.com/wp/wp-content/uploads/2020/02/parasite-film-tribute-1.jpg',
+    year: 2019,
+    score: 0  
+  }
+]
+
+
+
